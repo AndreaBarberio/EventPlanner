5989caabd60fbdb4acf0594c89d7cad37855d0b4
Andrea Barberio <99880853+AndreaBarberio@users.noreply.github.com>
2023-07-06 15:48:25 +0200

    feat: configure and install jest and eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div className="container">
-
-      </div>
+      <div className="container"></div>
     </div>
   );
 }
