df4dcab256dad5d129c91285787c2efbb490ae89
Andrea Barberio <99880853+AndreaBarberio@users.noreply.github.com>
2023-07-10 12:31:22 +0200

    feat: initialie Login Page feat: initialize routes chore: install react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import './output.css';
+import { Route, Routes } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
 function App() {
-	return <div className="App bg-red-200">ciao</div>;
+	return (
+		<Routes>
+			<Route path="/" element={<LoginPage />} />
+		</Routes>
+	);
 }
 
 export default App;
---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,5 @@
+const LoginPage = () => {
+	return <>This is the login page</>;
+};
+
+export default LoginPage;
